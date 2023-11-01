f1e3b379c444b17004d5d4a2f69b5d2242a3ee2d
mailofk <mailofk@naver.com>
2023-11-01 22:07:03 +0900

    initial commit

---
+++ src/main/java/com/kk/TeamUp/TeamUpApplication.java
@@ -0,0 +1,13 @@
+package com.kk.TeamUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamUpApplication.class, args);
+	}
+
+}
