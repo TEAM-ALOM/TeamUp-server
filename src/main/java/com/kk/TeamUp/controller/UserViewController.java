8aa3b12c1188ba1e97df910fb264ef978ef42ccb
mailofk <mailofk@naver.com>
2023-11-09 16:41:08 +0900

    userview

--- src/main/java/com/kk/TeamUp/controller/UserViewController.java
+++ src/main/java/com/kk/TeamUp/controller/UserViewController.java
@@ -1,18 +1,48 @@
 package com.kk.TeamUp.controller;
 
 
+import com.kk.TeamUp.domain.User;
+import com.kk.TeamUp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class UserViewController {
+    private final UserService userService;
+
     @GetMapping("/login")
     public String Login() {
         return "/login";
     }
 
-    @GetMapping("/hihi")
-    public String Hihi() {
-        return "/hihi";
+    @GetMapping("/CreateTest")
+    public String Created() {
+        return "/CreateTest";
+    }
+
+    @GetMapping("/DeleteTest")
+    public String Deleted() {
+        return "/DeleteTest";
+    }
+
+    @GetMapping("/users")
+    public String findAllUsers(Model model) {
+        List<User> users = userService.findAllUsers();
+        model.addAttribute("users",users);
+        return "/users";
+    }
+
+    @GetMapping("/users/{id}")
+    public String findUser(@PathVariable long id,  Model model) {
+        User user = userService.findUser(id);
+        model.addAttribute("user",user);
+        return "/oneUser";
     }
 }
